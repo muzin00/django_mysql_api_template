2ce0c6353706a6ae2462aab3c18a410389dc66b5
yusuke <muzin00@gmail.com>
2024-03-15 23:54:12 +0900

    init

---
+++ docker/api/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Waiting for MySQL..."
+
+while ! nc -z $DB_HOST $DB_PORT; do
+    sleep 0.1
+done
+
+echo "MySQL started"
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
